1401a959e9ef5194c606bc6911a46488d5cdead2
Nadya Hristuk <elhe2013@gmail.com>
2024-04-03 15:14:57 +0200

    Update routers.js

--- routers/routers.js
+++ routers/routers.js
@@ -6,9 +6,9 @@ const {
   getCoinByPrice,
   getCoinByPriceChange1h,
   getCoinByPriceChange24h,
-  getCoinByPriceChange7d,
+  getCoinByPriceChange7d,getCoinList,
 } = require("../controllers/controllers");
-
+router.get("/list", getCoinList);
 router.get("/name/:name", getCoinByName);
 router.get("/symbol/:symbol", getCoinBySymbol);
 router.get("/price/:sortOrder", getCoinByPrice);
